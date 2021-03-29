9a34db2b8665140304bead4d2441861c67bcd451
Fahrenheitru <ifahrenheitru@gmail.com>
2021-03-30 01:37:36 +0500

    feat: add functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let tri, res;
+  let totalStr = [];
+  if (typeof members !== "string" && !Array.isArray(members)) {
+    return false;
+  } else {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === "string") {
+        tri = members[i].trim().toUpperCase();
+        totalStr.push(tri[0]);
+      }
+    }
+    res = totalStr.sort().join("");
+    return res;
+  }
 };
